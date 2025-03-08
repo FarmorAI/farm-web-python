ff036a321be251b9b413cca5e57f2831d173d94d
hogi2020 <junmyung2020@gmail.com>
2025-03-09 00:35:22 +0900

    update api for model serving

--- src/services/ImagePreprocessor.py
+++ src/services/ImagePreprocessor.py
@@ -12,14 +12,14 @@ class ImagePreprocessor:
       # 1. 이미지 크기 조정
       image = cv2.resize(image, self.target_size)
 
-      # 3. 샤프닝 필터 정의 (일반적인 샤프닝 커널)
+      # 2. 샤프닝 필터 정의 (일반적인 샤프닝 커널)
       mask = np.asarray([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype = np.float32)
       image = cv2.filter2D(image, -1, mask)
 
-      # 4. 이미지 정규화
+      # 3. 이미지 정규화
       image = image.astype(np.float32) / 255.0
 
-      # 5. 배치 차원 추가
+      # 4. 배치 차원 추가
       # 모델의 입력은 batch 단위로 처리되기 때문에, 
       # 단일 이미지를 모델에 전달하려면 batch_size=1로 설정해줘야 합니다.
       # 모델이 여러 개의 샘플을 처리하는 방식을 기대하는데, 
