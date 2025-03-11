f880867c0df79f9b59232a3c284de3ec53c8939f
dlekgp1183 <dlekgp1183@naver.com>
2025-03-11 12:33:05 +0900

    mainpage graph completed

--- src/repository/MySqlRepository.py
+++ src/repository/MySqlRepository.py
@@ -1,38 +1,39 @@
-from fastapi import FastAPI
-import numpy as np
+from fastapi import HTTPException
 import pandas as pd
-import scipy.stats as stats
 import mysql.connector
-from fastapi.middleware.cors import CORSMiddleware
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class MySqlRepository:
-    def __init__(self, DB_CONFIG):
-        self.db_config = DB_CONFIG
-
+    def __init__(self):
+        self.pool = mysql.connector.pooling.MySQLConnectionPool(
+            pool_name="mypool",
+            pool_size=10,
+            host = os.getenv("DB_HOST"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            database = os.getenv("DB_NAME")
+        )
     def get_data(self):
-        conn = mysql.connector.connect(self.db_config)
-        cursor = conn.cursor(dictionary=True)
-        
-        query = "SELECT cate3, weight FROM apple_label_dataset"
-        cursor.execute(query)
-        data = cursor.fetchall()
-
-        cursor.close()
-        conn.close()
-
-        df = pd.DataFrame(data)
-        grade_map = {"특": 2, "상": 1, "보통": 0}
-        df["cate3"] = df["cate3"].map(grade_map)
-
-        return df
-
-
-DB_CONFIG = {
-    "host": "192.168.0.4",
-    "user": "farmorai_admin",
-    "password": "farmorai12345",
-    "database": "farmdb"
-}
-
+        try:
+            conn = self.pool.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            query = "SELECT cate3, weight FROM apple_label_dataset"
+            cursor.execute(query)
+            data = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            df = pd.DataFrame(data)
+            if df.empty:
+                raise ValueError("No data retrieved from database")
+            grade_map = {"특": 2, "상": 1, "보통": 0}
+            df["cate3"] = df["cate3"].map(grade_map)
+            return df
+        except Exception as e:
+            print(f"Database error: {str(e)}")
+            raise HTTPException(
+                status_code=500,
+                detail=f"Database error: {str(e)}"
+            )
 # 싱글톤
-mysql_repository = MySqlRepository(DB_CONFIG)
+mysql_repository = MySqlRepository()
